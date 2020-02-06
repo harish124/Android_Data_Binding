c631321144af2dac32b8b2f6e900fb532c70433b
harish124 <balajiharish4@gmail.com>
2020-02-06 20:24:37 +0530

    Refactored CashIt app

--- ShapesApp/app/src/main/java/com/example/shapesapp/MainActivity.java
+++ ShapesApp/app/src/main/java/com/example/shapesapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.shapesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
+
+import com.example.shapesapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
     }
 }
